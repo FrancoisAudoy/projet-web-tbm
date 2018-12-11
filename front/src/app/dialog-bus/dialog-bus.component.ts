2a7a057aedc15a02ae930858eb4303c667ef0c5e
FrancoisAudoy <francois.audoy@gmail.com>
2018-12-11 16:20:43 +0100

    Les arrêts possibles sont filtré en fonction de numéro de ligne

--- front/src/app/dialog-bus/dialog-bus.component.ts
+++ front/src/app/dialog-bus/dialog-bus.component.ts
@@ -9,11 +9,31 @@ import { Arret } from '../arret';
   styleUrls: ['./dialog-bus.component.css']
 })
 export class DialogBusComponent implements OnInit {
-  private line: string;
+  private line: number;
   private stop: string;
+  private lineNumber: number[] = [];
+  private allStopName: string[] = [];
+  private allStop: Arret [] = [];
+ 
 
   constructor(public dialRef: MatDialogRef<DialogBusComponent>,
-    @Inject(MAT_DIALOG_DATA) private data: Arret) { }
+    @Inject(MAT_DIALOG_DATA) private data: Arret[]) {
+      this.allStop = data;
+    data.forEach(element => {
+      if (!this.lineNumber.includes(parseInt(element.lineName)))
+        this.lineNumber.push(parseInt(element.lineName));
+    });
+  }
+
+  lineChange(){
+    console.log("Change " + this.line);
+    let filtredStop = this.allStop.filter(x => parseInt(x.lineName) == this.line);
+
+    filtredStop.forEach(element => {
+      this.allStopName.push(element.name);
+    });
+    
+  }
 
   ngOnInit() {
   }
